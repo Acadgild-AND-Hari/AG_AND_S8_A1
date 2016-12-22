32912e358218dfd45022c6a66e33db48ac7c80e4
harinivaskumar <harinivaskumar@live.com>
2016-12-23 03:28:35 +0530

    Code Commit
    Initial code commit

---
+++ app/src/main/java/com/hari/aag/userpreferences/UserPreferencesActivity.java
@@ -0,0 +1,13 @@
+package com.hari.aag.userpreferences;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserPreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_preferences);
+    }
+}
